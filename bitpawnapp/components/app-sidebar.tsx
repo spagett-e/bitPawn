ef03966e32f3f079d508c21c1179029a2c680417
spagett-e <stefant.6127@gmail.com>
2025-02-03 19:49:58 +0800

    update app-sidebar.tsx

--- bitpawnapp/components/app-sidebar.tsx
+++ bitpawnapp/components/app-sidebar.tsx
@@ -4,15 +4,58 @@ import {
   SidebarFooter,
   SidebarGroup,
   SidebarHeader,
-} from "@/components/ui/sidebar"
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components it/ui/sidebar"
+import { Gamepad2, Info, ShieldCheck, Cog } from "lucide-react"
+ 
+
+// Menu items.
+const items = [
+  {
+    title: "Play",
+    url: "#",
+    icon: Gamepad2,
+  },
+  {
+    title: "About",
+    url: "#",
+    icon: Info,
+  },
+  {
+    title: "Security & Transparency",
+    url: "#",
+    icon: ShieldCheck,
+  },
+  {
+    title: "Settings",
+    url: "#",
+    icon: Cog,
+  },
+]
 
 export function AppSidebar() {
   return (
     <Sidebar>
       <SidebarHeader />
       <SidebarContent>
-        <SidebarGroup />
-        <SidebarGroup />
+        <SidebarGroup>
+        <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <a href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
       </SidebarContent>
       <SidebarFooter />
     </Sidebar>
