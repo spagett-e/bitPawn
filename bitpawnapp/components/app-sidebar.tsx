948d44329daaa77320057c442c85508e9c4c7324
Spagett <196570193+spagett-e@users.noreply.github.com>
2025-02-03 13:03:15 +0000

    modified:   bitpawnapp/components/app-sidebar.tsx

--- bitpawnapp/components/app-sidebar.tsx
+++ bitpawnapp/components/app-sidebar.tsx
@@ -40,24 +40,24 @@ export function AppSidebar() {
   return (
     <Sidebar>
       <SidebarHeader />
-      <SidebarContent>
-        <SidebarGroup>
-        <SidebarGroupContent>
-          <SidebarMenu>
-              {items.map((item) => (
-                <SidebarMenuItem key={item.title}>
-                  <SidebarMenuButton asChild>
-                    <a href={item.url}>
-                      <item.icon />
-                      <span>{item.title}</span>
-                    </a>
-                  </SidebarMenuButton>
-                </SidebarMenuItem>
-              ))}
-            </SidebarMenu>
-          </SidebarGroupContent>
-        </SidebarGroup>
-      </SidebarContent>
+       <SidebarContent>
+         <SidebarGroup>
+           <SidebarGroupContent>
+             <SidebarMenu>
+               {items.map((item) => (
+                 <SidebarMenuItem key={item.title}>
+                   <SidebarMenuButton asChild>
+                     <a href={item.url}>
+                       <item.icon />
+                       <span>{item.title}</span>
+                     </a>
+                   </SidebarMenuButton>
+                 </SidebarMenuItem>
+               ))}
+             </SidebarMenu>
+           </SidebarGroupContent>
+         </SidebarGroup>
+       </SidebarContent>
       <SidebarFooter />
     </Sidebar>
   )
