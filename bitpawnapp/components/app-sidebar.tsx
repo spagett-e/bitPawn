496a8876d7e1eb17f63c080470f8594a65d46560
spagett-e <stefant.6127@gmail.com>
2025-02-04 07:04:13 +0800

    update app-sidebar.tsx

--- bitpawnapp/components/app-sidebar.tsx
+++ bitpawnapp/components/app-sidebar.tsx
@@ -40,7 +40,23 @@ export function AppSidebar() {
   const{ isMobile } = useSidebar()
   return (
     <Sidebar variant="floating" collapsable="icon">
-      <SidebarHeader />
+      <SidebarHeader>
+       <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton size="lg" asChild>
+              <a href="#">
+                <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground">
+                  <GalleryVerticalEnd className="size-4" />
+                </div>
+                <div className="flex flex-col gap-0.5 leading-none">
+                  <span className="font-semibold">Documentation</span>
+                  <span className="">v1.0.0</span>
+                </div>
+              </a>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarHeader>
        <SidebarContent>
          <SidebarGroup>
            <SidebarGroupContent>
