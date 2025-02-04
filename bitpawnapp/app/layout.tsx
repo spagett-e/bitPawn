20d8e6acd1565842a586a5826c391af68e954b42
Spagett <196570193+spagett-e@users.noreply.github.com>
2025-02-04 02:33:47 +0000

    update

--- bitpawnapp/app/layout.tsx
+++ bitpawnapp/app/layout.tsx
@@ -1,20 +1,10 @@
 import { AppSidebar } from "@/components/app-sidebar"
 import {
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbLink,
-  BreadcrumbList,
-  BreadcrumbPage,
-  BreadcrumbSeparator,
-} from "@/components/ui/breadcrumb"
-import { Separator } from "@/components/ui/separator"
-import {
-  SidebarInset,
   SidebarProvider,
   SidebarTrigger,
 } from "@/components/ui/sidebar"
 
-export default function Layout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
     <SidebarProvider>
       <AppSidebar />
